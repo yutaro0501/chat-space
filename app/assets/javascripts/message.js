eaf9f5d1e26b21874175592366a7830e6151aa0d
yutaroyoshida <yoshida19950501@gmail.com>
2019-10-27 20:35:12 +0900

    modified-code

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,7 +35,7 @@ $('#new_message').on('submit', function(e) {
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
-    $('#message_content').val('');
+    $('form')[0].reset();
     $('#send-button').prop('disabled', false);
     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
   })
