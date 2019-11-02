500d5d9352aed847ecdbfb360daed53ad36f6aa2
yutaroyoshida <yoshida19950501@gmail.com>
2019-11-02 20:36:26 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module ChatSpace
       g.test_framework false
     end
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :utc
+    config.active_record.default_timezone = :local
   end
 end
