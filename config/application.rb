82c596f053ca7525076f8847e09612861bce21bf
yutaroyoshida <yoshida19950501@gmail.com>
2019-11-02 20:00:50 +0900

    timezone

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -16,7 +17,7 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
     end
+    config.time_zone = 'Tokyo'
   end
 end
